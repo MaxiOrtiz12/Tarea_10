e0f0ba497a788ac896bd224942b3954c92b53f15
maxiortiz12 <maxiortizorigami@gmail.com>
2023-05-18 16:19:02 -0300

    actividad 10

---
+++ js/js.js
@@ -0,0 +1,17 @@
+var nombre= "maxi";
+var apellido= "ortiz";
+
+console.log (nombre + " " + apellido);
+
+var numero1=2
+var numero2=3
+var resultadosuma=numero1+numero2;
+
+console.log("el resultado de la suma es", + resultadosuma);
+
+var numero3=1
+console.log("resultado de suma", numero1 + numero2 + numero3)
+
+console.log(nombre+" "+numero2)
+
+console.log()
